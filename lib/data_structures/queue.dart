933cdf893113aafe4ed32c778e440b441f9add01
Kaio C. de Oliveira <hello@kaio.dev>
2025-02-08 19:52:36 -0300

    feat: implement QueueLinkedList class using DoublyLinkedList

--- lib/data_structures/queue.dart
+++ lib/data_structures/queue.dart
@@ -1,3 +1,5 @@
+import 'package:dart_data_structures_and_algorithms/data_structures/doubly_linked_list.dart';
+
 abstract class Queue<E> {
   bool enqueue(E element);
   E? dequeue();
@@ -43,4 +45,42 @@ void main() {
 
   print(queue.peek); // Second
   print(queue); // [Second, Third]
+
+  print('QueueLinkedList example');
+  final queueLinkedList = QueueLinkedList<String>();
+
+  queueLinkedList.enqueue('First');
+  queueLinkedList.enqueue('Second');
+  queueLinkedList.enqueue('Third');
+
+  print(queueLinkedList); // [First, Second, Third]
+
+  queueLinkedList.dequeue();
+  print(queueLinkedList); // [Second, Third]
+
+  print(queueLinkedList.peek); // Second
+
+  print(queueLinkedList); // [Second, Third]
+}
+
+class QueueLinkedList<E> implements Queue<E> {
+  final _list = DoublyLinkedList<E>();
+
+  @override
+  E? dequeue() => _list.pop();
+
+  @override
+  bool enqueue(E element) {
+    _list.append(element);
+    return true;
+  }
+
+  @override
+  bool get isEmpty => _list.isEmpty;
+
+  @override
+  E? get peek => _list.head?.value;
+
+  @override
+  toString() => _list.toString();
 }
